74d8664843cb8e35b4105205561d7cea711c38fa
mousa haddadin <mousahaddadin@gmail.com>
2022-04-09 13:28:50 -0500

    fixed aggregate methods

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -2,10 +2,11 @@
 const { ObjectId } = require("mongoose").Types;
 const { Student, Course } = require("../models");
 
-// TODO: Create an aggregate function to get the number of students overall
+//aggregate function to get the number of students overall
 const headCount = async () =>
   Student.aggregate()
-    // Your code here
+
+    .count("studentCount")
     .then((numberOfStudents) => numberOfStudents);
 
 // Execute the aggregate method on the Student model and calculate the overall grade by using the $avg operator
@@ -13,14 +14,16 @@ const grade = async (studentId) =>
   Student.aggregate([
     // TODO: Ensure we include only the student who can match the given ObjectId using the $match operator
     {
-      // Your code here
+      $match: { _id: ObjectId(studentId) },
     },
     {
       $unwind: "$assignments",
     },
-    // TODO: Group information for the student with the given ObjectId alongside an overall grade calculated using the $avg operator
     {
-      // Your code here
+      $group: {
+        _id: ObjectId(studentId),
+        overallGrade: { $avg: "$assignments.score" },
+      },
     },
   ]);
 
@@ -31,7 +34,7 @@ module.exports = {
       .then(async (students) => {
         const studentObj = {
           students,
-          // headCount: await headCount(),
+          headCount: await headCount(),
         };
         return res.json(studentObj);
       })
